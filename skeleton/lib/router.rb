0320bfb2ed1eef995cd01a13841b7c7ef6d33d34
pzengpzeng <steveninouye@msn.com>
2018-11-21 17:32:40 -0800

    Add Route#initialize for phase 5 of rails lite app

--- skeleton/lib/router.rb
+++ skeleton/lib/router.rb
@@ -1,16 +1,27 @@
+require 'byebug'
+
 class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern
+    @http_method = http_method
+    @controller_class = controller_class
+    @action_name = action_name
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    req.path =~ @pattern
   end
 
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
+    if matches?(req) && (req.request_method == @http_method)
+      controller = @controller_class.new
+      controller.invoke_action(@action_name)
+    end
   end
 end
 
